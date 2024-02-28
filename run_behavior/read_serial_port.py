d3681da56d142d53fb3ca5878e3be5f710b6e2a7
DJESTRIN <david.james.estrin@outlook.com>
2024-02-28 16:20:30 -0500

    updating serial port reader code

--- run_behavior/read_serial_port.py
+++ run_behavior/read_serial_port.py
@@ -1,16 +1,37 @@
 #Import dependencies
 import serial.tools.list_ports #pyserial must be installed
+import serial
 from csv import writer
 import keyboard
+import os, time
 
 class recordport():
-    def __init__(self,output_directory=None,readrate=9600,selected_port=None):
-        self.output_directory=output_directory
-        self.readrate=9600 
+    def __init__(self,output_directory=None,output_filename=None,readrate=9600,selected_port=None):
+        """ recordport class: used to record data from a port such as from an arduino computer
+        output_directory = str or None, default path to where data will be saved
+        output_filename = str or None, default CSV file where data will be saved in output_directory
+        readrate = int, number of times comp will read port per second
+        selected_port = str or None, the specific port we will listen to. Example input is 'COM3'
+        """
+        self.output_directory=output_directory 
+        self.output_filename=output_filename 
+        self.readrate=9600  
         self.selected_port=selected_port
         self.sSerial = serial.Serial()
+        self.messages_to_user()
 
     def messages_to_user(self):
+        #Set up directory and file for writing data
+        if self.output_directory==None:
+            self.set_up_output_directory()
+        else:
+            if not os.path.exists(self.output_directory):
+                os.makedir(self.output_directory)
+
+        if self.output_filename==None:
+            self.set_up_output_file()
+
+        # Set up port for reading
         if self.selected_port != None:
             print(f'You have manually entered {self.selected_port} as the device of interest')
             print(f'We will read this device at a rate of {self.readrate} per second')
@@ -22,8 +43,17 @@ class recordport():
             self.find_port()
             print(f'We will read this device at a rate of {self.readrate} per second')
 
-    def set_up_output_file():
-        self.output_file=
+        self.record_port()
+
+    def set_up_output_directory(self):
+        current_time = time.strftime("%Y%m%d_%H%M%S")
+        currentdir=os.getcwd()
+        self.output_directory=f'{currentdir}/tmp{current_time}/'
+        os.makedir(self.output_directory)
+                    
+    def set_up_output_file(self):
+        current_time = time.strftime("%Y%m%d_%H%M%S")
+        self.output_file=f'portrecording{current_time}.csv'
 
     def find_port(self):
         ports = serial.tools.list_ports.comports()
@@ -37,15 +67,18 @@ class recordport():
 
         for x in range(0,len(portList)):
             if portList[x].startswith("COM"+str(correct_port)):
-                portVar="COM"+str(correct_port)
+                self.selected_port="COM"+str(correct_port)
                 print(f'This is our selected port: {portList[x]}')
 
         self.sSerial.baudrate=self.readrate
-        self.sSerial.port=portVar
+        self.sSerial.port=self.selected_port
         self.sSerial.open()
 
-
-    def record_port():
+    def record_port(self):
+        print(f'Now trying to read port from {self.selected_port}')
+        print(f'Type ctrl + shift + d to stop recording')
+        
+        self.output_file=os.path.join(self.output_directory,self.output_file) #Set final output file to its full path
         running=True
         while running:
             if sSerial.in_waiting:
