6df8690eb92fc4cd5edfbae62431a7b04e177160
DJESTRIN <david.james.estrin@outlook.com>
2024-02-29 17:08:15 -0500

    updating gui

--- run_behavior/read_serial_port.py
+++ run_behavior/read_serial_port.py
@@ -7,6 +7,7 @@ import os, time
 import argparse
 import PySimpleGUI as sg
 import tkinter as tk
+from tkinter import *
 
 class recordport():
     def __init__(self,output_directory=None,output_filename=None,readrate=9600,selected_port=None):
@@ -21,41 +22,6 @@ class recordport():
         self.readrate=readrate  
         self.selected_port=selected_port
         self.sSerial = serial.Serial()
-
-    def __call__(self):
-        #Actions to take when object is called
-        self.user_interface()
-
-    def user_interface(self):
-        #Set up directory and file for writing data
-        if self.output_directory==None:
-            self.set_up_output_directory()
-        else:
-            if not os.path.exists(self.output_directory):
-                os.makedir(self.output_directory)
-
-        if self.output_filename==None:
-            self.set_up_output_file()
-
-        # Set up port for reading
-        if self.selected_port != None:
-            print(f'You have manually entered {self.selected_port} as the device of interest')
-            print(f'We will read this device at a rate of {self.readrate} per second')
-            self.sSerial.baudrate=self.readrate
-            self.sSerial.port=self.selected_port
-            self.sSerial.open()
-        else:
-            print(f'No device selected, lets find the device you are interested in....')
-            self.find_port()
-            print(f'We will read this device at a rate of {self.readrate} per second')
-
-        self.record_port()
-
-    def set_up_output_directory(self):
-        current_time = time.strftime("%Y%m%d_%H%M%S")
-        currentdir=os.getcwd()
-        self.output_directory=f'{currentdir}/tmp{current_time}/'
-        os.mkdir(self.output_directory)
                     
     def set_up_output_file(self):
         current_time = time.strftime("%Y%m%d_%H%M%S")
@@ -69,133 +35,125 @@ class recordport():
             portList.append(str(port_oh))
             print(str(port_oh))
         self.portList=portList
-        # correct_port = input("Please input the correct COM value as an integer. For example, if COM27 is the computer of interest, input the int 27. Go!:")
 
-        # for x in range(0,len(portList)):
-        #     if portList[x].startswith("COM"+str(correct_port)):
-        #         self.selected_port="COM"+str(correct_port)
-        #         print(f'This is our selected port: {portList[x]}')
+    def setserial(self,index,running):
+        self.sSerial[index].baudrate=self.baudlist[index]
+        self.sSerial[index].port=self.portList[index]
+        self.sSerial[index].open()
 
-        # self.sSerial.baudrate=self.readrate
-        # self.sSerial.port=self.selected_port
-        # self.sSerial.open()
-
-    def record_port(self):
-        print(f'Now trying to read port from {self.selected_port}')
-        print(f'Type ctrl + shift + d to stop recording')
-        
-        self.output_file=os.path.join(self.output_directory,self.output_file) #Set final output file to its full path
-        running=True
         while running:
-            if self.sSerial.in_waiting:
-                packet=self.sSerial.readline()
+            if self.sSerial[index].in_waiting:
+                packet=self.sSerial[index].readline()
                 decoded_packet=packet.decode('utf').rstrip('\n')
-                print(decoded_packet) #Print so user can see whats being saved
 
                 #Write decoded packet to file
                 with open(self.output_file, 'a') as fileob:
                     wro = writer(fileob)
                     wro.writerow(decoded_packet)
-                    fileob.close()
-            
-            if keyboard.is_pressed("ctrl+shift+d"):
-                running=False
-                break
+                    fileob.close()    
 
 class GUI(recordport):
     def __init__(self):
         super().__init__() #Use same inputs as before
         self.root=tk.Tk()
-        self.root.geometry("1000x1000")
+        self.root.geometry("1000x800")
         self.root.configure(bg="black")
+
+    def __call__(self):
+        self.load_images()
+        self.set_up_layout()
+        self.refresh_screen()
+    
+    def load_images(self):
+        #Get all images used by app
+        self.logo = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\logo.png") #Logo
+        OnImage = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\on.png") #On off switch
+        offImage = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\off.png")#Off switch
+        sensimage = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\sens.png") #Used for sens synce switch
+        syncimage = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\sync.png") #Used for sens synce switch
+        offsso = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\off_sso.png") #Used for sens synce switch
+
+        # Resample images for specs of gui
+        self.offImage = offImage.subsample(4, 4)
+        self.logo = self.logo.subsample(5, 5)
+        self.OnImage = OnImage.subsample(4, 4)
+        self.sensimage = sensimage.subsample(3, 3)
+        self.syncimage = syncimage.subsample(3, 3)
+        self.offsso = offsso.subsample(3, 3)
+
+    def set_up_layout(self):
         self.title = tk.Label(text="Port Reader",foreground="white", background="black")
         self.title.config(font=("Arial", 25))
-        self.title.pack()
-        #self.root.mainloop()
-    
-    def __call__(self):
-        self.set_up_buttons()
-        #self.file_save_entries()
-        self.root.mainloop()
+        self.title.place(x=450,y=10)
+        #self.logo = tk.Label(image=self.logo)
+        #self.logo.place(x=10,y=0)
+        self.browse_button = tk.Button(text="Set Output Folder",height=1,width=20, bg='black',fg='white',font= ('Arial 10 bold'), command=self.get_directory)
+        self.browse_button.place(x=10,y=50)
+        self.findportsbutton = tk.Button(self.root, text='Search for COMs', height=1, width=20, bg='black',fg='white', font= ('Arial 10 bold'), command=self.find_port)
+        self.findportsbutton.place(x=10,y=90) 
 
     def refresh_screen(self):
         self.root.mainloop()
 
-    def set_up_buttons(self):
-        self.browse_button = tk.Button(text="Set Output Folder",
-                                       height=1,width=20, bg='black',fg='white', 
-                                       font= ('Arial 10 bold italic'),
-                                       command=self.get_directory)
-        self.browse_button.place(x=10,y=50)
-        self.findportsbutton = tk.Button(self.root, text='Search for COMs', 
-                                         height=1, width=20, bg='black',fg='white', 
-                                         font= ('Arial 10 bold italic'),
-                                         command=self.find_port)
-        self.findportsbutton.place(x=10,y=90) 
+    def find_port(self):
+        super().find_port()
+        self.set_run_buttons(len(self.portList))
+        return
 
     def get_directory(self):
         directory = tk.filedialog.askdirectory()
         string=f'Output directory: {directory}'
         self.showdir = tk.Label(text=string,foreground="white", background="black")
         self.showdir.config(font=("Arial", 15))
-        self.showdir.place(x=200,y=45)
+        self.showdir.place(x=200,y=200)
         self.root.mainloop()
         return directory
-    
-    def find_port(self):
-        super().find_port()
-        #Set subtitle to display all the coms
-        self.subtitle=tk.Label(text="Here are the following COMS: ", fg="white",bg="black")
-        self.subtitle.config(font=("Arial", 15))
-        start=120
-        self.subtitle.place(x=10, y=start)
 
-        self.subtitle2=tk.Label(text="Select Arduino type", fg="white",bg="black")
-        self.subtitle2.config(font=("Arial", 15))
-        self.subtitle2.place(x=500, y=start)
+    def run_recording(self,index):
+        state_oh = self.run_btn_lst[index]
+        if state_oh=='on':
+            self.recordbuttons[index].config(state='normal', image = self.offImage)
+            self.run_btn_lst[index] = 'off'
+            print(f'I, button number {index}, am off now')
+        else:
+            self.recordbuttons[index].config(state='normal', image = self.OnImage)
+            self.run_btn_lst[index] = 'on'
+            print(f'I, button number {index}, am on now')
+            self.display_recording()
 
-        self.subtitle3=tk.Label(text="Set Baud Rate", fg="white",bg="black")
-        self.subtitle3.config(font=("Arial", 15))
-        self.subtitle3.place(x=700, y=start)
+    def comp_type(self,index):
+        state_oh = self.type_btn_lst[index]
+        if state_oh=='off':
+            self.typebuttons[index].config(state='normal', image = self.sensimage)
+            self.type_btn_lst[index] = 'sens'
+            print(f'I, button number {index}, am sens now')
+        elif state_oh=='sens':
+            self.typebuttons[index].config(state='normal', image = self.syncimage)
+            self.type_btn_lst[index] = 'sync'
+            print(f'I, button number {index}, am sync now')
+        else:
+            self.typebuttons[index].config(state='normal', image = self.offsso)
+            self.type_btn_lst[index] = 'off'
+            print(f'I, button number {index}, am off now')
 
-        for i,port in enumerate(self.portList):
-            start+=30
-            # Show the COM name
-            self.showdir = tk.Label(text=port,foreground="white", background="black")
-            self.showdir.config(font=("Arial", 15))
-            self.showdir.place(x=10,y=start)
+    def set_run_buttons(self,n):
+        self.run_btn_lst=['off'] * n
+        self.type_btn_lst=['off'] * n
+        self.recordbuttons=[]
+        self.typebuttons=[]
+        starty=300
+        for index in range(n):
+            #Set up the run button per comp
+            btn=Button(self.root,image = self.offImage, highlightthickness = 0, bd = 0, borderwidth=0,command=lambda k=index:self.run_recording(k))
+            btn.place(x=600,y=starty)
+            self.recordbuttons.append(btn)
 
-            #Generate radio button options
-            self.rb=tk.Checkbutton(self.root,text="sync",fg='white',bg='black',selectcolor='black')
-            self.rb.config(font=("Arial", 15))
-            self.rb.place(x=500,y=start-3)
-            self.rb=tk.Checkbutton(self.root,text="sensor",fg='white',bg='black',selectcolor='black')
-            self.rb.config(font=("Arial", 15))
-            self.rb.place(x=580,y=start-3)
+            #Set up the type button per comp
+            btn=Button(self.root,image = self.offsso,highlightthickness = 0, bd = 0, borderwidth=0, command=lambda k=index:self.comp_type(k))
+            btn.place(x=50,y=starty)
+            self.typebuttons.append(btn)
 
-            #Generate baud rate
-            self.textBox = tk.Entry(self.root) 
-            self.textBox.place(x=700,y=start+2)
-            self.textBox.insert(0, '9600') 
-
-            #Generate filename options to save csv file to
-            self.textBox = tk.Entry(self.root) 
-            self.textBox.place(x=850,y=start+2)
-            example_filename=(f'Arduino_{i}.csv')
-            self.textBox.insert(0, example_filename) 
-
-        #Set the run button
-        self.run_button = tk.Button(self.root, text="RUN",
-                                       height=3,width=20, bg='red',fg='black', 
-                                       font= ('Arial 10 bold'),
-                                       command=self.run_all)
-        self.run_button.place(relx=0.9,rely=0.9,anchor='center')
-
-        self.refresh_screen()
-
-    def run_all(self):
-        print('Running')
-
+            starty+=80
 
         
 if __name__=='__main__':
--- run_behavior/testgui.py
+++ run_behavior/testgui.py
@@ -1,34 +1,75 @@
 import tkinter as tk
+from tkinter import *
+import ipdb
+
 class GUI():
     def __init__(self):
+        #super().__init__() #Use same inputs as before
         self.root=tk.Tk()
-        self.root.geometry("1000x1000")
+        self.root.geometry("1000x800")
+        self.portList=20
         self.root.configure(bg="black")
-        self.title = tk.Label(text="Kenneth Johnson's Port Reader",foreground="white", background="black")
-        self.title.config(font=("Arial", 20))
-        self.title.pack()
+
+    def __call__(self):
+        self.load_images()
+        self.set_up_layout()
+        self.set_run_buttons(self.portList)
+        self.refresh_screen()
+    
+    def load_images(self):
+        #Get all images used by app
+        self.logo = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\logo.png") #Logo
+        OnImage = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\on.png") #On off switch
+        offImage = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\off.png")#Off switch
+        sensimage = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\sens.png") #Used for sens synce switch
+        syncimage = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\sync.png") #Used for sens synce switch
+        offsso = PhotoImage(file = "D:\\twophoton\\run_behavior\\gui\\off_sso.png") #Used for sens synce switch
+
+        # Resample images for specs of gui
+        self.offImage = offImage.subsample(7, 7)
+        self.logo = self.logo.subsample(5, 5)
+        self.OnImage = OnImage.subsample(7, 7)
+        sensimage = sensimage.subsample(7, 7)
+        syncimage = syncimage.subsample(7, 7)
+        offsso = offsso.subsample(7, 7)
+
+    def set_up_layout(self):
+        self.title = tk.Label(text="Port Reader",foreground="white", background="black")
+        self.title.config(font=("Arial", 25))
+        self.title.place(x=100,y=50)
+        self.logo = tk.Label(image=self.logo)
+        self.logo.place(x=10,y=0)
+
+    def refresh_screen(self):
         self.root.mainloop()
+
+    def switch(self,index):
+        state_oh = btn_lst[index]
+        if state_oh=='on':
+            self.buttons[index].config(state='normal', image = self.offImage)
+            btn_lst[index] = 'off'
+            print(f'I, button number {index}, am off now')
+        else:
+            self.buttons[index].config(state='normal', image = self.OnImage)
+            btn_lst[index] = 'on'
+            print(f'I, button number {index}, am on now')
+
+    def set_run_buttons(self,n):
+        global btn_lst
+        btn_lst=['off'] * n
+        self.buttons=[]
+        starty=0
+        for index,state in enumerate(btn_lst):
+            btn=Button(self.root,image = self.offImage, command=lambda k=index:self.switch(k))
+            btn.place(x=100,y=starty)
+            self.buttons.append(btn)
+            starty+=20
 
 if __name__=='__main__':
     goh=GUI()
-
-# from tkinter import filedialog
-# from tkinter import *
-
-# def browse_button():
-#     # Allow user to select a directory and store it in global var
-#     # called folder_path
-#     filename = filedialog.askdirectory()
-#     folder_path.set(filename)
-#     print(filename)
+    goh()
 
 
-# root = Tk()
-# folder_path = StringVar()
-# lbl1 = Label(master=root,textvariable=folder_path)
-# lbl1.grid(row=0, column=1)
-# button2 = Button(text="Browse", command=browse_button)
-# button2.grid(row=0, column=3)
 
 
 
