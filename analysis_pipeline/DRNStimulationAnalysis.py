ec47f5e4b4d44a3689d7ae56ba85581486d84ebf
DJESTRIN <david.james.estrin@outlook.com>
2024-04-09 19:00:33 -0400

    updating pipelines

---
+++ analysis_pipeline/DRNStimulationAnalysis.py
@@ -0,0 +1,11 @@
+"""
+DRN analysis
+(1) AUC between each stim. (How long for ITI, get entire ITI. )
+    During the stims. --> look into transform 
+    compare to baseline period before
+    compare to post stim period after
+    normalize by time. 
+    
+(2) Correlation between neurons. 
+    pre, during, between and post.
+"""
