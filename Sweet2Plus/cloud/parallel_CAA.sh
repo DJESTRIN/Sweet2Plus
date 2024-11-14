04b8fa0ca9caa460e3cf0cda095a1e23a4cf980b
David <david.james.estrin@outlook.com>
2024-11-14 16:38:37 -0500

    Updating cloud based code

--- Sweet2Plus/cloud/parallel_CAA.sh
+++ Sweet2Plus/cloud/parallel_CAA.sh
@@ -10,11 +10,15 @@ echo -e "Searching in $root_data_folder \
     and will run sbatch script $sbatch_script"
 
 # Use find to locate folders matching the pattern and submit them to sbatch
+counter=0
 for beh_folder in $(find "$root_data_folder" -type d -name "$folder_name_pattern"); do
     image_folder=$(find "$beh_folder" -type d -name '*_R*')
 
     if [ -n "$image_folder" ]; then
         echo "Submitted $beh_folder and $image_folder to sbatch."
+        counter=$((counter + 1)) 
     fi
     #sbatch "$sbatch_script" "$beh_folder" "$image_folder"
 done
+
+echo "Submitted a total of $counter jobs for $sbatch_script"
