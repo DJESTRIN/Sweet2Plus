8176604f52742485366ddf1e3559473a77fe9c2b
David <david.james.estrin@outlook.com>
2024-11-27 14:03:43 -0500

    Reorganizing weight modeling code

--- Sweet2Plus/modeling/weightmodels.py
+++ Sweet2Plus/modeling/weightmodels.py
@@ -31,6 +31,7 @@ Questions:
 (4) Plotting code to show predicted vs reality for randomly sampled neurons
 (5) Plot weights across neurons across conditions ...
 """
+
 # Import dependencies
 import argparse
 from Sweet2Plus.core.SaveLoadObjs import LoadObj
@@ -39,140 +40,361 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+import optuna
+import os
 import ipdb
 
-# Custom functions and classes
+""" Data wrangling and set up functions and classes """
 def zscore_data_from_obj(obj_file_oh):
+    # Pull z scored neuronal trace data from object and load into numpy array
     objoh = LoadObj(FullPath=obj_file_oh)
-    return objoh.recording
+    return np.asarray(objoh.ztraces).T
 
 class datawrangler(torch.utils.data.Dataset):
-    def __init__(self,z_scored_neural_data,test_size=0.2,random_state=43):
+    """ Wrangles data from z score neuronal trace data into the correct format for ANN """
+    def __init__(self,z_scored_neural_data,test_size=0.2,random_state=43,X_points=25,cheat_mode=False):
         self.zdata=z_scored_neural_data
         self.test_size=test_size
         self.random_state=random_state
+        self.X_points=X_points
+        self.cheat_mode=cheat_mode
 
         # Default methods
-        self.X,self.y=self.rearrange_data()
+        self.normalize_data()
+        if self.cheat_mode:
+            self.X,self.y=self.get_cheating_data()
+        else:
+            self.X,self.y=self.rearrange_data()
     
+    def normalize_data(self):
+        zdatanorm=[]
+        for trace in self.zdata.T:
+            neuron_data = (trace-trace.min())/(trace.max()-trace.min())
+            zdatanorm.append(neuron_data)
+        self.zdata=np.array(zdatanorm).T
+
     def __call__(self):
-        X_train, y_train, X_val, y_val, X_test, y_test = self.split()
-        return X_train, y_train, X_val, y_val, X_test, y_test
+        X_train, y_train, X_test, y_test = self.split()
+        return X_train, y_train, X_test, y_test
 
     def rearrange_data(self):
-        X,y=[],[]
-        for t1,t2 in zip(self.zdata[:-1],self.zdata[1:]):
-            X.append(t1), y.append(t2)
+        X, y = [], []
+        
+        # Iterate over the data with a sliding window
+        for i in range(len(self.zdata) - (self.X_points + 1)):
+            # Select X_points for input and the subsequent value for output
+            stackoh = np.vstack(self.zdata[i:i + self.X_points])
+            target = self.zdata[i + self.X_points + 1]
+
+            X.append(stackoh)
+            y.append(target)
+        
         return X, y
+    
+    def get_cheating_data(self):
+        # Set X and y to the exact same value
+        X, y = [], []
+        for i in range(len(self.zdata) - (self.X_points + 1)):
+            stackoh = self.zdata[i + self.X_points + 1] # Set X and Y to the exact same value
+            target = self.zdata[i + self.X_points + 1]
+
+            X.append(stackoh)
+            y.append(target)
+        return X, y
+        
+    def get_original_data(self):
+        return self.X, self.y
     
     def split(self):
         X_train,X_test,y_train,y_test = train_test_split(self.X,self.y,test_size=self.test_size,random_state=self.random_state)
-        X_train,X_val,y_train,y_val = train_test_split(X_train,y_train,test_size=self.test_size,random_state=self.random_state)
-        return X_train, y_train, X_val, y_val, X_test, y_test
+        return X_train, y_train, X_test, y_test
+
+""" Custom Neuronal Network layers and Architectures"""
+class DirectInputLayer(nn.Module):
+    """ A Custom Layer where M inputs are directly input into N neurons,
+      This is notably not a dense layer.  """
+    def __init__(self, network_size, inputs_per_neuron):
+        super(DirectInputLayer, self).__init__()
+        self.network_size = network_size
+        self.inputs_per_neuron = inputs_per_neuron
+
+        # Create separate linear transformations for each neuron
+        self.linear = nn.Linear(inputs_per_neuron, 1, bias=True)
+    
+    def forward(self,x):
+        # Ensure tensor is contiguous before reshaping
+        x = x.contiguous()  # Add this
+        x = self.linear(x) 
+        return x.squeeze(-1)
 
 class SingleLayerNetwork(torch.nn.Module):
-    def __init__(self, network_size):
+    """ A Single Layer network utalizing DirectInputLayer """
+    def __init__(self, network_size,inputs_per_neuron,dropout):
         super(SingleLayerNetwork, self).__init__()
-        self.lin = torch.nn.Linear(in_features=network_size, out_features=network_size)  
-        self.relu=torch.nn.ReLU()
+        # Set up layers for neural network
+        self.direct_input_layer = DirectInputLayer(network_size, inputs_per_neuron)
+        self.relu1 = torch.nn.LeakyReLU(negative_slope=0.5)
+        self.drop1 = torch.nn.Dropout(p=dropout)
+        self.dense_layer = torch.nn.Linear(network_size, network_size)  
+        #self.relu2 = torch.nn.LeakyReLU(negative_slope=0.5)
     
     def forward(self,xoh):
-        return self.relu(self.lin(xoh))
+        xoh = self.direct_input_layer(xoh)
+        xoh = self.relu1(xoh)
+        xoh = self.drop1(xoh)
+        xoh = self.dense_layer(xoh)
+        #xoh = self.relu2(xoh)
+        return xoh
+    
+class SimpleSingleLayerNetwork(torch.nn.Module):
+    """ A Single Layer network utalizing DirectInputLayer """
+    def __init__(self, network_size,inputs_per_neuron,dropout):
+        super(SingleLayerNetwork, self).__init__()
+        # Set up layers for neural network
+        self.dense_layer1 = torch.nn.Linear(network_size, network_size)  
+        self.relu1 = torch.nn.LeakyReLU(negative_slope=0.5)
+        self.drop1 = torch.nn.Dropout(p=dropout)
+        self.dense_layer2 = torch.nn.Linear(network_size, network_size)  
 
-def training(model, train_loader, val_loader, criterion, optimizer, num_epochs=10, device="cuda"):
-    # Move the model to the specified device
-    model.to(device)
+    def forward(self,xoh):
+        xoh = self.dense_layer1(xoh)
+        xoh = self.relu1(xoh)
+        xoh = self.drop1(xoh)
+        xoh = self.dense_layer2(xoh)
+        return xoh
 
-    # Store loss history
-    loss_history = {'train': [], 'val': []}
+""" Primary analysis pipeline for training, testing and debugging ANN """
+class weightmodel_pipeline():
+    """ Builds pipeline needed to train network """
+    def __init__(self, data, model=[], num_data_points=25, epochs=100, print_training=True,
+                 print_training_epoch=50, plot_neurons=False, run_study=False, cheat_mode=False, 
+                 device='cuda',neuron_fig_path=r'C:\Users\listo\Sweet2Plus\my_figs\neuron_predictions',
+                 main_fig_path=r'C:\Users\listo\Sweet2Plus\my_figs'):
+        """
+        Inputs:
 
-    for epoch in range(num_epochs):
-        # Training phase
-        model.train()
-        train_loss = 0.0
-        for inputs, targets in train_loader:
-            inputs, targets = inputs.to(device), targets.to(device)
+        """
+        # Set initial attributes
+        self.data = data
+        self.model = model
+        self.num_data_points = num_data_points
+        self.device = device
+        self.epochs = epochs
+        self.print_training_epoch = print_training_epoch
+        self.print_training = print_training
+        self.plot_neurons = plot_neurons
+        self.neuron_fig_path = neuron_fig_path
+        self.main_fig_path=main_fig_path
+        self.run_study = run_study
+        self.cheat_mode = cheat_mode
 
-            # Zero the gradient buffers
-            optimizer.zero_grad()
+    def __call__(self):
+        """ General protocol for pipeline """
+        if not self.model and not self.run_study:
+            user_answer=input('No model was given and run_study is false, \
+                              would you like to run a study (yes or no)? \
+                              If no, please re-run with model.','yes','no')
 
-            # Forward pass
-            outputs = model(inputs)
+            if user_answer=='yes':
+                self.run_study=True
+            else:
+                print('Without a model included and no study chosen, there is nothing to run. Terminating code... :(')
+                return
 
-            # Compute loss
-            loss = criterion(outputs, targets)
+        # Determine if NN should be run in cheat mode as a validation
+        if self.cheat_mode:
+            print("Running a model in cheat mode! Results should be nearly perfrect")
+            # run training 
+            # plot learning curve
+            # run testing
+            # Save results
+            return 
 
-            # Backward pass and optimizer step
-            loss.backward()
-            optimizer.step()
+        # Determine if NN should be run via a study to find best hyperparmeters
+        if self.run_study:
+            print("Running a hyperparameter study via optuna ... ")
+            print(" Relax, this might take a while ... ")
+            # run study
+            # get best hyperparameters
+            # run training 
+            # plot learning curve
+            # run testing
+            # Save results
+            return 
+        
+        # Run a regular model
+        if not self.run_study:
+            print("Running the given model ... ")
+            # run training 
+            # plot learning curve
+            self.plot_learning_curve(data=history, filename=r'C:\Users\listo\Sweet2Plus\my_figs\optuna_best_model_history.jpg')
+            # run testing
+            # Save results
+            return
 
-            # Accumulate training loss
-            train_loss += loss.item() * inputs.size(0)
+    def get_data(self,num_data_points):
+        arranged_data = datawrangler(z_scored_neural_data=self.data,X_points=num_data_points)
+        X_train, y_train, X_test, y_test = arranged_data()
+        X_original, y_original=arranged_data.get_original_data()
+        return X_train, y_train, X_test, y_test, X_original, y_original
+    
+    def convert_to_torch_loader(self,X_train, y_train, X_test, y_test, X_original, y_original):
+        # Put data into torch formatting
+        X_train = torch.tensor(np.asarray(X_train), dtype=torch.float32)
+        y_train = torch.tensor(np.asarray(y_train), dtype=torch.float32)
+        X_test = torch.tensor(np.asarray(X_test), dtype=torch.float32)
+        y_test = torch.tensor(np.asarray(y_test), dtype=torch.float32)
+        xo = torch.tensor(np.asarray(X_original), dtype=torch.float32)
+        yo = torch.tensor(np.asarray(y_original), dtype=torch.float32)
 
-        # Average training loss
-        train_loss /= len(train_loader.dataset)
-        loss_history['train'].append(train_loss)
+        train_dataset = TensorDataset(X_train, y_train)
+        test_dataset = TensorDataset(X_test, y_test)
+        od = TensorDataset(xo, yo)
 
-        # Validation phase
-        model.eval()
-        val_loss = 0.0
-        with torch.no_grad():
-            for inputs, targets in val_loader:
-                inputs, targets = inputs.to(device), targets.to(device)
+        train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+        val_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+        original_loader = DataLoader(od, batch_size=1, shuffle=False)
+        return train_loader, val_loader, original_loader
+    
+    def train(self, model, train_loader, val_loader, criterion, optimizer, scheduler):
+        """ Run training on given model """
+        # Send model to gpu
+        model.to(self.device)
 
-                # Forward pass
+        # Record training and validation loss
+        loss_history = {'train': [], 'val': []}
+
+        for epoch in range(self.epochs):
+            model.train()
+            train_loss = 0.0
+            for inputs, targets in train_loader:
+                inputs = inputs.transpose(1, 2).contiguous() # Reconfigure input shape
+                inputs, targets = inputs.to(self.device), targets.to(self.device) # Send data to gpu
+                optimizer.zero_grad()
                 outputs = model(inputs)
-
-                # Compute loss
                 loss = criterion(outputs, targets)
+                loss.backward()
+                optimizer.step()
+                train_loss += loss.item() * inputs.size(0)
 
-                # Accumulate validation loss
-                val_loss += loss.item() * inputs.size(0)
+            # Update learning rate via scheduler
+            scheduler.step() 
 
-        # Average validation loss
-        val_loss /= len(val_loader.dataset)
-        loss_history['val'].append(val_loss)
+            # Update training loss
+            train_loss /= len(train_loader.dataset)
+            loss_history['train'].append(train_loss)
 
-        print(f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
+            # Validation phase
+            model.eval()
+            val_loss = 0.0
+            with torch.no_grad():
+                for inputs, targets in val_loader:
+                    inputs = inputs.transpose(1, 2).contiguous()
+                    inputs, targets = inputs.to(self.device), targets.to(self.device)
+                    outputs = model(inputs)
+                    loss = criterion(outputs, targets)
+                    val_loss += loss.item() * inputs.size(0)
 
-    return model, loss_history
+            val_loss /= len(val_loader.dataset)
+            loss_history['val'].append(val_loss)
+
+            if self.print_training:
+                if epoch%self.print_training_epoch==0:
+                    print(f"Epoch {epoch}/{self.epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
+
+        return model, loss_history
+
+    def test(self, modeloh, test_loader):
+        real_data=[]
+        predict_data=[]
+        for inputs, targets in test_loader:
+            inputs = inputs.transpose(1, 2).contiguous()
+            inputs = inputs.to(self.device)
+            outputs = modeloh(inputs)
+            outputs_numpy = outputs.cpu().detach().numpy()
+            targets=targets.detach().numpy()
+            real_data.append(targets)
+            predict_data.append(outputs_numpy)
+
+        real_data=np.squeeze(np.asarray(real_data)).T
+        predict_data=np.squeeze(np.asarray(predict_data)).T
+        correlation = np.corrcoef(real_data, predict_data)[0, 1]
+
+        if self.plot_neurons:
+            for neuron_id in range(len(real_data)):
+                plt.figure(figsize=(10,10))
+                plt.plot(real_data[:,neuron_id], label='Real Output')
+                plt.plot(predict_data[:,neuron_id], label='Model Output')
+                plt.xlabel('time')
+                plt.ylabel('Z_F')
+                filepath = os.path.join(self.neuron_fig_path,f"neuron{neuron_id}.jpg")
+                plt.savefig(filepath)
+                plt.close()
+        
+        return correlation
+
+    def plot_learning_curve(self,data,filename):
+        plt.figure()
+        plt.plot(data['train'])
+        plt.plot(data['val'])
+        plt.savefig(filename)
+        plt.close()
+
+    def hypertuning_study(self):
+        """Utalize Optuna to run a study to find best hyperparameters """
+        def objective(trial):
+            # Hyperparameter suggestions
+            learning_rate = trial.suggest_loguniform('learning_rate', 1e-6, 1e-1)
+            weight_decay = trial.suggest_loguniform('weight_decay', 1e-6, 1e-2)
+            dropout_rate = trial.suggest_uniform('dropout_rate', 0.0, 0.5)
+            numberXpoints = trial.suggest_int('numberXpoints', 2, 300)
+
+            # Get neural data from file and seperate into X_train, y_train, etc
+            X_train, y_train, X_test, y_test, X_original, y_original = self.get_data(num_data_points=numberXpoints)
+            train_loader, val_loader, original_loader = self.convert_to_torch_loader(X_train, y_train, X_test, y_test, X_original, y_original)
+            
+            # Build model
+            model_oh = SingleLayerNetwork(network_size=X_train.shape[2],inputs_per_neuron=X_train.shape[1],dropout=dropout_rate)
+
+            # Set up criterion and optimizers
+            criterion_oh = nn.MSELoss()
+            optimizer = optim.Adam(model_oh.parameters(), lr=learning_rate, weight_decay=weight_decay)
+            scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+
+            # Run training and get loss history
+            trained_model, history = self.train(model_oh, train_loader, val_loader, criterion_oh, optimizer, scheduler)
+
+            # Run testing and get correlation between signals
+            corroh = self.test(model_oh, original_loader)
+
+            # Maximize correlation
+            return corroh
+
+        # Run Optuna study
+        self.study = optuna.create_study(direction='maximize')
+        self.study.optimize(objective, n_trials=30, n_jobs=4)
+
+        best_hyperparameters = self.study.best_params
+        self.learning_rate = best_hyperparameters['learning_rate']
+        self.weight_decay = best_hyperparameters['weight_decay']
+        self.dropout_rate = best_hyperparameters['dropout_rate']
+        self.numberXpoints = best_hyperparameters['numberXpoints']
+        return 
 
 def cli_parser():
+    # Get command line arguments
     parser=argparse.ArgumentParser()
     parser.add_argument('--s2p_object_file',type=str,help='Directory to a custom s2p object')
+    parser.add_argument('--cheat_mode',action='store_true',help='When included, model will be given easy data that it should be able to get 100 accuracy')
+    parser.add_argument('--hypertuning_study',action='store_true',help='Run hyperparameter study with optuna')
     args = parser.parse_args()
     return args
 
-def main():
-    # Parse command line inputs
-    args = cli_parser() 
-
-    # Get neural data from file and seperate into X_train, y_train, etc
-    zscore_data = zscore_data_from_obj(args.s2p_object_file) # Get z score data from file
-    arranged_data = datawrangler(z_scored_neural_data=zscore_data)
-    X_train, y_train, X_val, y_val, X_test, y_test = arranged_data()
-
-    # Put data into torch formatting
-    train_dataset = TensorDataset(X_train, y_train)
-    val_dataset = TensorDataset(X_val, y_val)
-    test_dataset = TensorDataset(X_test, y_test)
-
-    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
-
-    # Build model
-    number_of_neurons = len(X_train) # Need to fix this
-    model_oh = SingleLayerNetwork(network_size=number_of_neurons)
-
-    # Set up criterion and optimizers
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model_oh.parameters(), lr=0.001)
-
-    # Run training
-    trained_model, history = training(model_oh, train_loader, val_loader, criterion, optimizer, num_epochs=10, device="cuda")
-
-
-
 if __name__=='__main__':
-    main()
+    args = cli_parser()
+    zdataoh = zscore_data_from_obj(args.s2p_object_file)
+    ob = datawrangler(z_scored_neural_data=zdataoh,cheat_mode=True)
+    X_train, y_train, X_test, y_test = ob()
+    ipdb.set_trace()
