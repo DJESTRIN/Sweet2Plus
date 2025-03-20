1ddc13c0e359b5782d1b4a749dee1baa01d3da9d
David <david.james.estrin@outlook.com>
2025-03-20 14:04:52 -0400

    update to kenneth code

--- Kenneth/Calcium_analysis.py
+++ Kenneth/Calcium_analysis.py
@@ -10,47 +10,34 @@ from multiprocessing import Pool
 from math import dist as dist
 import ipdb
 
+def GetNumpyArray(parent_directory):
+    Fnp = np.load(os.path.join(parent_directory, 'F.npy'))
+    ICnp = np.load(os.path.join(parent_directory, 'iscell.npy'))
+    return Fnp, ICnp
 
-# Step 1: Define the directory where your .npy file is located
-input_directory = r"\\Kenneth-NAS\data\25-3-12\25-3-12\25-3-12_C4856077_M1_SERT_Flp_chrmine_drn_G6M-mpfc_15C_15stim_r1-042\suite2p\plane0"
-# Replace with your desired path
-npy_filename = 'F.npy'  # Replace with your .npy file name
+def FilterCells(Fnp, ICnp):
+    ipdb.set_trace()
+    return Fnp
 
-# Step 2: Load the .npy file from the specific directory
-npy_file_path = os.path.join(input_directory, npy_filename)
-npy_data = np.load(npy_file_path)
+def GetStats(Fnp, timepoints = [(0,738),(739,1439),(1440,-1)]):
+    ipdb.set_trace()
+    area = np.trapz(y,x)
 
-# Step 3: Convert the NumPy array to a pandas DataFrame (optional but useful)
-df = pd.DataFrame(npy_data)
-print (df.shape)
+    return AUCs, Traces
 
-#Select a specific row in the dataframe 
-y = df.iloc[0,:]
-x = pd.Series(range(1, 1001))
-#x = np.arange(len(y))
- 
+def GeneratePlots(AUCs, Traces):
+    plt.plot(x, y, label="Y = 2X")
+    plt.show()
 
-#Plot 
-plt.plot(x, y, label="Y = 2X")
-plt.show()
+if __name__=='__main__':
+    # Get parent directory and create drop directory for dataframe and images 
+    parent_directory = r"\\Kenneth-NAS\data\25-3-12\25-3-12\25-3-12_C4856077_M1_SERT_Flp_chrmine_drn_G6M-mpfc_15C_15stim_r1-042\suite2p\plane0" 
+    drop_directory = r"\\Kenneth-NAS\data\25-3-12\25-3-12\25-3-12_C4856077_M1_SERT_Flp_chrmine_drn_G6M-mpfc_15C_15stim_r1-042\suite2p\analysis" 
+    os.makedirs(drop_directory, exist_ok=True)
 
-area = np.trapz(y,x)
-print (area)
-ipbd.set_trace()
+    # Run current file through analysis. 
+    FnpOH, ICnpOH = GetNumpyArray(parent_directory=parent_directory)
+    FnpOH = FilterCells(FnpOH, ICnpOH)
+    AUCsOH, TracesOH = GetStats(FnpOH, drop_directory)
+    GeneratePlots(AUCsOH, TracesOH, drop_directory)
 
-
-# Step 4: Define the directory where you want to save the CSV file
-output_directory = r"\\Kenneth-NAS\data\25-3-12\25-3-12\25-3-12_C4856077_M1_SERT_Flp_chrmine_drn_G6M-mpfc_15C_15stim_r1-042\suite2p\plane0"  # Replace with your desired path
-
-# Ensure the output directory exists, if not create it
-os.makedirs(output_directory, exist_ok=True)
-
-# Step 5: Specify the full path for the CSV file
-csv_filename = 'output_file.csv'  # Name of the output .csv file
-csv_file_path = os.path.join(output_directory, csv_filename)
-
-# Step 6: Save the DataFrame to the specified CSV file
-df.to_csv(csv_file_path, index=False)
-
-# Optional: Confirm the file was saved successfully
-print(f"CSV file has been saved to {csv_file_path}")
