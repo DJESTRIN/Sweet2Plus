91b4004d65d2ba1ed186ca6bfd9c91be4de3332f
David <david.james.estrin@outlook.com>
2025-12-30 09:46:04 -0500

    updated slurm memory requirement to 256 Gb

--- Sweet2Plus/SynapticWeightModeling/WeightModelingSpinup.py
+++ Sweet2Plus/SynapticWeightModeling/WeightModelingSpinup.py
@@ -21,7 +21,7 @@ def parse_cli_inputs():
 
 class spinup:
     def __init__(self,parent_input_directory, slurm_output_directory, job_name='SynapticWeightModeling', 
-                 partition='scu-gpu', gpus=1, memory=128, conda_environment='sweet2p'):
+                 partition='scu-gpu', gpus=1, memory=256, conda_environment='sweet2p'):
         # Directory containing all s2p data objects
         self.parent_input_directory = parent_input_directory
 
